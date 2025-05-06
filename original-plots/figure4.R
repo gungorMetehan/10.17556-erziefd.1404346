b3465d8bf8168b7ad87645af5ebfe1d58da6421b
Metehan GÜNGÖR <102655648+gungorMetehan@users.noreply.github.com>
2025-05-06 13:29:48 +0300

    Add files via upload

---
+++ original-plots/figure4.R
@@ -0,0 +1,20 @@
+# importing dataset
+
+# changing variable names (only for fixing legend title problem)
+colnames(data_sci) <- c("country","se","var_betweenschool", "Weighting Status")
+
+# drawing plot1
+plot1 <- ggplot(data_sci, aes(x = country, y = se, color = `Weighting Status`)) +
+  geom_point(aes(shape = `Weighting Status`, size = 1.5)) +
+  labs(x = "Country", y = "Standard Error") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 0)) +
+  scale_colour_manual(values = c("#ee1289","#eec900", "#1e8fff", "black")) +
+  guides(size = F)
+
+plot1
+
+
+
+
+
+        
