1b7db06e77253c032f885c44d25825d797b42630
Metehan GÜNGÖR <102655648+gungorMetehan@users.noreply.github.com>
2025-05-06 14:46:11 +0300

    Update figure3.R

--- original-plots/figure3.R
+++ original-plots/figure3.R
@@ -6,10 +6,9 @@ colnames(data_math) <- c("country","se","var_betweenschools", "Weighting Status"
 # drawing plot1
 library(ggplot2)
 plot3 <- ggplot(data_math, aes(x = country, y = se, color = `Weighting Status`)) +
-  geom_point(aes(shape = `Weighting Status`, size = 1.5)) +
+  geom_point(aes(shape = `Weighting Status`), size = 2.5) +
   labs(x = "Country", y = "Standard Error") +
   theme(axis.text.x = element_text(angle = 90, hjust = 0)) +
-  scale_colour_manual(values = c("#ee1289","#eec900", "#1e8fff", "black")) +
-  guides(size = F)
+  scale_colour_manual(values = c("#ee1289","#eec900", "#1e8fff", "black"))
 
 plot3
